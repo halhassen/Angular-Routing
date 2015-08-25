3395a949a952ceca15bf707104c01a0732dd1d54
Hamad Alhassen <halhassen@gmail.com>
2015-08-25 01:14:07 -0700

    more angular basics

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,38 +1,8 @@
-angular.module('app', []) //brings in non-angular dependencies
-/*.controller('OldController', ['$scope', function($scope) { //brings in dependencies that are brought in from angular
-	$scope.popup = function() {
-		$scope.thing = $scope.stuff;
-	};
-}])*/
-.controller('OtherController', [function() {
+angular.module('app', []) 
+.controller("FormController", [/* scope would go here*/function() {
 	var vm = this;
-	vm.newCar = "";
-	vm.cars = [];
-	vm.editing = false;
-	vm.editCar = {};
-	vm.create = function() {
-		vm.cars.push(vm.newCar);
-		vm.newCar = "";
+	vm.user = {};
+	vm.register = function() {
+		console.log(vm.user);
 	}
-	
-	vm.startEdit = function(index) {
-		vm.editing = true;
-		vm.editCar.place = index;
-		vm.editCar.name = vm.cars[index];
-	}
-
-	vm.submitEdit = function(car) {
-		vm.cars[vm.editCar.place] = vm.editCar.name;
-		vm.editing = false;
-	}
-
-	vm.exp = function() {
-		if((1+1) === 2) {
-			return true;
-		}
-		else {
-			return false;
-		}
-		//ng-if REMOVES it from the DOM tree, not hides it
-	}
-}]);
+}])
